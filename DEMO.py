76703c341b13b30aec9dff2d1db178354fc708c7
MrPranav1800 <pranav.raj.rg00@mail.com>
2023-01-03 21:36:25 +0530

    done the changes

--- DEMO.py
+++ DEMO.py
@@ -8,3 +8,4 @@ print("This is the second print statement from Atharva")
 print("Also i did change the remote from Https to SSH :p")
 
 print ("pranav added to atharva's code")
+print("done by Pranav and roll no. is AM.EN.P2CSN22010")
