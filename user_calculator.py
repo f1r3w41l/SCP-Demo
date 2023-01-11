4b48a566fc413a416648305188a4f15abde8f246
f1r3w41l <atharvamane08@gmail.com>
2023-01-11 10:33:49 +0530

    No bugs now

--- user_calculator.py
+++ user_calculator.py
@@ -1,4 +1,18 @@
-print("Give me two numbers Sir/Mam -")
-a=int(input("Input the first number here - "))
-b=int(input("Input the first number here - "))
-print(f"This is your addition - {a+b} ")
+print("Give me two numbers Sir/Mam ")
+flag_a=1
+flag_b=1
+while flag_a==1 or flag_b==1:
+    a=input("Input the first number here - ")
+    if a.isnumeric():
+        x=int(a)
+        flag_a=0
+    else:
+        print("Enter a INTEGER NUMBER for A")
+    b=input("Input the second number here - ")
+    if b.isnumeric():
+        x=int(b)
+        flag_b=0
+    else:
+        print("Enter a INTEGER NUMBER for B")
+        
+print(f"This is your addition - {int(a)+int(b)} ")
